0e28a1adf3ce68fd6283d9458bc1124b95b8a805
Aluno <fernanda.costa@bandtec.com.br>
2021-04-19 22:22:22 -0300

    create recycleview

---
+++ PayShareApp/app/src/main/java/com/payshare/payshareapp/Activity_card_extrato.kt
@@ -0,0 +1,11 @@
+package com.payshare.payshareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_card_extrato : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_extrato)
+    }
+}
---
+++ PayShareApp/app/src/main/java/com/payshare/payshareapp/activity_card_sala_pagamento.kt
@@ -0,0 +1,11 @@
+package com.payshare.payshareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_card_sala_pagamento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_sala_pagamento)
+    }
+}
