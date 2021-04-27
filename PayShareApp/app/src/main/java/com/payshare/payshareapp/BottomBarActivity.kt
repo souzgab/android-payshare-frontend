170b1a100e0fab14e294e0abec7e8eb49eacf0d6
unknown <gabriel.arruda@bandtec.com.br>
2021-04-26 23:45:30 -0300

    fix: setting data threat on login and sends to correct page

--- PayShareApp/app/src/main/java/com/payshare/payshareapp/BottomBarActivity.kt
+++ PayShareApp/app/src/main/java/com/payshare/payshareapp/BottomBarActivity.kt
@@ -4,6 +4,7 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 import com.google.gson.Gson
@@ -18,23 +19,33 @@ class BottomBarActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_bar)
 
+        preferencias = getSharedPreferences("Auth", MODE_PRIVATE)
+        var lobbySession = preferencias.getString("idLobby", null)
+
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        bottomNavigation.show(0)
 
         bottomNavigation.add(MeowBottomNavigation.Model(0, R.drawable.ic_home))
-        bottomNavigation.add(MeowBottomNavigation.Model(1, R.drawable.ic_carteira))
+        bottomNavigation.add(MeowBottomNavigation.Model(1, R.drawable.extrato))
         bottomNavigation.add(MeowBottomNavigation.Model(2, R.drawable.ic_logo))
         bottomNavigation.add(MeowBottomNavigation.Model(3, R.drawable.ic_carteira))
         bottomNavigation.add(MeowBottomNavigation.Model(4, R.drawable.ic_perfil))
 
-        mudarTela(HubFragment.newInstance())
+
+        if (lobbySession != null) {
+            bottomNavigation.show(2, true)
+            mudarTela(Sala_PagamentoFragment.newInstance())
+            supportActionBar?.hide()
+        } else {
+            bottomNavigation.show(0, true)
+            mudarTela(HubFragment.newInstance())
+        }
 
         bottomNavigation.setOnClickMenuListener {
-
+            supportActionBar?.show()
             preferencias = getSharedPreferences("Auth", MODE_PRIVATE)
-            val lobbySession = preferencias.getString("idLobby", null)
+            lobbySession = preferencias.getString("idLobby", null)
 
             when(it.id){
                 0 -> {
@@ -46,6 +57,7 @@ class BottomBarActivity : AppCompatActivity() {
                 2 -> {
                     if (lobbySession != null) {
                         Log.e("Sucesso", "Lobby não esta nula$lobbySession")
+                        supportActionBar?.hide()
                         mudarTela(Sala_PagamentoFragment.newInstance())
                     } else {
                         Log.e("Sucesso", "Lobby esta nula$lobbySession")
@@ -65,7 +77,6 @@ class BottomBarActivity : AppCompatActivity() {
             }
         }
     }
-
 
     private fun mudarTela(fragment: Fragment) {
         val fragmentTransition = supportFragmentManager.beginTransaction()
